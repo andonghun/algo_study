339e0829ddf82cc5639dcf79e222b9f255b4afa1
SongHayoung <lovelydays95@naver.com>
2020-02-11 14:23:16 +0900

    _

--- 프로그래머스/04. 완전탐색/모의고사_송하영.cpp
+++ 프로그래머스/04. 완전탐색/모의고사_송하영.cpp
@@ -1,49 +1,22 @@
-#include <string>
 #include <vector>
 #include <math.h>
-#include <iostream>
 using namespace std;
-int number[10]={0,};
-int answer = 0,size;
-int cur_size;
-void calc(int d,int &s,vector<bool> &Primes){
-    if(d==cur_size){
-        if((s&1)&Primes[s/2]){
-            ++answer;
-        }
-        return ;
-    }
-    for(int i=0;i<=9;i++){
-        if(number[i]!=0){
-            --number[i];    s = s * 10 + i;
-            calc(d + 1, s, Primes);
-            ++number[i];    s /= 10;
-        }
-    }
-}
 
-int solution(string numbers) {
-    size = numbers.length();
-    int MAXLEN = pow(10,size);
-    vector<bool> Primes(MAXLEN/2,true);
-    for(int i=0;i<size;i++)
-        ++number[numbers[i]-48];
+vector<int> solution(vector<int> answers) {
+    int correct[3]={0,};
+    int div[3] = {5,8,10};
+    vector<vector<int>> solv = {{1,2,3,4,5},
+        {2,1,2,3,2,4,2,5},
+        {3,3,1,1,2,2,4,4,5,5}};
+    for(int i=0;i<answers.size();i++)
+        for(int j=0;j<3;j++)
+            if(answers[i]==solv[j][i%div[j]])
+                ++correct[j];
     
-    Primes[0] = false;
-    for(long long i=3;i*i<MAXLEN;i+=2){
-        if(!Primes[i/2])
-            continue;
-        for(long long j=i*i;j<MAXLEN;j+=i*2)
-            Primes[j/2] = false;
-    }
-    if(number[2]>0)
-        ++answer;
-    for(cur_size=1;cur_size<=size;cur_size++)
-        for (int i = 1; i <= 9; i++)
-            if (number[i] != 0) {
-                --number[i];
-                calc(1, i, Primes);
-                ++number[i];
-            }
+    int max_correct = max(correct[0],max(correct[1],correct[2]));
+    vector<int> answer;
+    for(int i=0;i<3;i++)
+        if(correct[i]==max_correct)
+            answer.push_back(i+1);
     return answer;
 }
