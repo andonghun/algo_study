ea09c62d87432a2cc7e01ca4337bbe8dd72e10df
minkyung <swkang1994@nate.com>
2020-02-25 12:22:38 +0900

    Create  n으로표현_강민경.cpp

---
+++ 프로그래머스/09. 동적계획법/n으로표현_강민경.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int answer = 9;
+
+void dfs(int N, int number, int count, int currentNumber){
+    
+    if(count >=9) return;
+    if(number == currentNumber){
+        answer = min(answer, count);
+        return;
+    }
+    
+    int tempNumber = 0;
+    for(int i=0; i + count < 9; i++){
+        tempNumber = tempNumber * 10 + N;
+        // N~ NNNN....
+        dfs(N, number, count + 1 + i, currentNumber + tempNumber);
+        dfs(N, number, count + 1 + i, currentNumber - tempNumber);
+        dfs(N, number, count + 1 + i, currentNumber * tempNumber);
+        dfs(N, number, count + 1 + i, currentNumber / tempNumber);
+
+    }
+}
+
+int solution(int N, int number) {
+    
+    // N, 현재숫자, 계산횟수
+    dfs(N, number, 0,0);
+    if(answer == 9) return -1;
+    return answer;
+}
