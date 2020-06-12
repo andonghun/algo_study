74fe8241c4035d39c8a7e8c72885bd81d38be4c7
진예진 <yeen0606@gmail.com>
2020-06-12 10:19:49 +0900

    Create 진예진_15651.cpp

---
+++ Season2/day02/진예진_15651.cpp
@@ -0,0 +1,42 @@
+/*
+2020-06-09 진예진
+[백준 15651] https://www.acmicpc.net/problem/15651
+*/
+
+#include <stdio.h>
+
+using namespace std;
+
+int N, M;
+int numbers[7];
+int selected_arr[7];
+
+void dfs(int index , int selected){
+    // 선택한 숫자의 개수가 M을 넘으면 이때까지 선택한 숫자를 출력한다.
+    if(selected == M){
+        for(int i = 0; i < M; i++)
+            printf("%d ", selected_arr[i]);
+        printf("\n");
+        return;
+    }
+
+    for(int i = 0; i < N; i++){
+        selected_arr[selected] = numbers[i];
+        dfs(i, selected + 1);
+    }
+}
+
+int main(){
+    FILE *stream = freopen("S2\\2\\15651_input.txt", "r", stdin);
+    if(!stream) perror("freopen");
+
+    scanf("%d %d", &N, &M);
+
+    for(int i = 0; i < N; i++){
+        numbers[i] = i+1;
+    }
+
+    dfs(0, 0);
+
+    return 0;
+}
